a666e8529403235757f349f42b3d38528c6c8400
Jeremy Johnson <jeremyjohnson@Jeremys-Mac-mini.local>
2022-07-26 18:33:15 -0600

    began calling the weather api and geo api to get locations and weather

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 var search = document.getElementById("search");
 
 
-function runClick(){
-    console.log("click");
+
+function runClick(cityName){
+    const city = document.getElementById("cityName").value;
+    findCity(city);
+    console.log(city);
+
+
+//     fetch("https://api.openweathermap.org/data/2.5/onecall?&exclude=minutely,hourly,alerts&appid=fe1835b3950936d6d47152d4217daee1"
+//   ).then(function(response) {
+//     console.log(response);
+//   });
 }
 
+function findCity(city) {
+    console.log(city);
+       fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city + ",1&limit=1&appid=fe1835b3950936d6d47152d4217daee1")
+       .then((response) => {
+        console.log(response);
+        return response.json();
+        
+    })
+       .then(data => console.log(data));
+
+       
+   
+}
+
+"http://api.openweathermap.org/geo/1.0/direct?q=Orem,1&limit=1&appid=fe1835b3950936d6d47152d4217daee1"
+
+
+
+// " + ValueFromFindCity + "
