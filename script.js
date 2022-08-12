ec3373d801fdbaa93e8f1865e6f53cfcd860e4cb
Jeremy Johnson <jeremyjohnson@Jeremys-Mac-mini.local>
2022-08-12 16:41:31 -0600

    insert data for 5 day forecast. Source weather images

--- script.js
+++ script.js
@@ -1,30 +1,86 @@
 var search = document.getElementById("search");
+var form = document.getElementById('form-submit');
+let mainCity = document.getElementById("mainCity");
+let mainTemp = document.getElementById("mainTemp");
+let mainWind = document.getElementById("mainWind");
+let mainHumid = document.getElementById("mainHumid");
+let mainUv = document.getElementById("mainUv");
+let d1date = document.getElementById("d1date");
+let d1image = document.getElementById("d1image");
+let d1temp = document.getElementById("d1temp");
+let d1wind = document.getElementById("d1wind");
+let d1humid = document.getElementById("d1humid");
+let d2date = document.getElementById("d2date");
+let d2image = document.getElementById("d2image");
+let d2temp = document.getElementById("d2temp");
+let d2wind = document.getElementById("d2wind");
+let d2humid = document.getElementById("d2humid");
+let d3date = document.getElementById("d3date");
+let d3image = document.getElementById("d3image");
+let d3temp = document.getElementById("d3temp");
+let d3wind = document.getElementById("d3wind");
+let d3humid = document.getElementById("d3humid");
+let d4date = document.getElementById("d4date");
+let d4image = document.getElementById("d4image");
+let d4temp = document.getElementById("d4temp");
+let d4wind = document.getElementById("d4wind");
+let d4humid = document.getElementById("d4humid");
+let d5date = document.getElementById("d5date");
+let d5image = document.getElementById("d5image");
+let d5temp = document.getElementById("d5temp");
+let d5wind = document.getElementById("d5wind");
+let d5humid = document.getElementById("d5humid");
 
 
+form.addEventListener('submit', (event) => runClick(event))
 
-async function runClick(cityName){
+
+async function runClick(event){
+  event.stopPropagation();
+  event.preventDefault();
+    console.log('run click');
     const city = document.getElementById("cityName").value;
     let cityData = await findCity(city);
-    let mainCity = document.getElementById("mainCity");
-    let mainTemp = document.getElementById("mainTemp");
-    let mainWind = document.getElementById("mainWind");
-    let mainHumid = document.getElementById("mainHumid");
-    let mainUv = document.getElementById("mainUv");
-
     console.log('city data');
     console.log(cityData);
-
-
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + cityData[0].lat +"&lon=" + cityData[0].lon +"&exclude=minutely,hourly,alerts&appid=fe1835b3950936d6d47152d4217daee1"
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + cityData[0].lat +"&lon=" + cityData[0].lon +"&units=imperial&exclude=minutely,hourly,alerts&appid=fe1835b3950936d6d47152d4217daee1"
   ).then(function(response) {
     return response.json()
   }).then(function(jsonData){
+    let date = new Date("MM-DD-YYYY");
+    console.log(date);
     console.log(jsonData);
-    mainCity.textContent = cityData[0].name;
-    mainTemp.textContent = jsonData.current.temp;
+    mainCity.textContent = cityData[0].name + " " + moment().format('L');
+    mainTemp.textContent = jsonData.current.temp + " \u00B0" + "F";
     mainWind.textContent = jsonData.current.wind_speed + " mph";
-    mainHumid.textContent = jsonData.current.humidity;
+    mainHumid.textContent = jsonData.current.humidity + "%";
     mainUv.textContent = jsonData.current.uvi;
+    d1date.textContent = moment().add(1, "d").format('L');
+    d1image.src = "http://openweathermap.org/img/wn/" + jsonData.daily[1].weather[0].icon +"@2x.png";
+    d1temp.textContent = "Temp: " + jsonData.daily[1].temp.day + " \u00B0" + "F";
+    d1wind.textContent = "Wind: " + jsonData.daily[1].wind_speed + " mph";
+    d1humid.textContent = "Humidity: " + jsonData.daily[1].humidity + "%";
+    d2date.textContent = moment().add(2, "d").format('L');
+    d2image.src = "http://openweathermap.org/img/wn/" + jsonData.daily[2].weather[0].icon +"@2x.png";
+    d2temp.textContent = "Temp: " + jsonData.daily[2].temp.day + " \u00B0" + "F";
+    d2wind.textContent = "Wind: " + jsonData.daily[2].wind_speed + " mph";
+    d2humid.textContent = "Humidity: " + jsonData.daily[2].humidity + "%";
+    d3date.textContent = moment().add(3, "d").format('L');
+    d3image.src = "http://openweathermap.org/img/wn/" + jsonData.daily[3].weather[0].icon +"@2x.png";
+    d3temp.textContent = "Temp: " + jsonData.daily[3].temp.day + " \u00B0" + "F";
+    d3wind.textContent = "Wind: " + jsonData.daily[3].wind_speed + " mph";
+    d3humid.textContent = "Humidity: " + jsonData.daily[3].humidity + "%";
+    d4date.textContent = moment().add(4, "d").format('L');
+    d4image.src = "http://openweathermap.org/img/wn/" + jsonData.daily[4].weather[0].icon +"@2x.png";
+    d4temp.textContent = "Temp: " + jsonData.daily[4].temp.day + " \u00B0" + "F";
+    d4wind.textContent = "Wind: " + jsonData.daily[4].wind_speed + " mph";
+    d4humid.textContent = "Humidity: " + jsonData.daily[4].humidity + "%";
+    d5date.textContent = moment().add(5, "d").format('L');
+    d5image.src = "http://openweathermap.org/img/wn/" + jsonData.daily[5].weather[0].icon +"@2x.png";
+    d5temp.textContent = "Temp: " + jsonData.daily[5].temp.day + " \u00B0" + "F";
+    d5wind.textContent = "Wind: " + jsonData.daily[5].wind_speed + " mph";
+    d5humid.textContent = "Humidity: " + jsonData.daily[5].humidity + "%";
+
   });
 }
 
